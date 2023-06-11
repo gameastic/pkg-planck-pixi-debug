f439408512208cae75769d41e4559644721a5d00
Ashot Avagyan <avagyanah@gmail.com>
2023-06-11 15:24:32 +0400

    rollback to tsc/parcel setup

--- packages/example/rollup-config.mjs
+++ packages/example/rollup-config.mjs
@@ -1,29 +1,34 @@
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import copy from 'rollup-plugin-copy';
-import livereload from 'rollup-plugin-livereload';
-import serve from 'rollup-plugin-serve';
+// // import commonjs from '@rollup/plugin-commonjs';
+// // import resolve from '@rollup/plugin-node-resolve';
+// // import copy from 'rollup-plugin-copy';
+// import esbuild from 'rollup-plugin-esbuild';
+// // import livereload from 'rollup-plugin-livereload';
+// // import serve from 'rollup-plugin-serve';
+// import typescript from 'rollup-plugin-typescript';
 
-export default [
-    {
-        input: 'src/index.ts',
+// export default [
+//     {
+//         input: 'src/index.ts',
 
-        output: [
-            //
-            { dir: 'dist', format: 'esm' },
-        ],
+//         output: [
+//             //
+//             { dir: 'dist', format: 'esm' },
+//         ],
 
-        plugins: [
-            //
-            typescript(),
-            commonjs({ sourceMap: false }),
-            resolve({ preferBuiltins: false }),
-            serve({ port: 8080 }),
-            livereload({ delay: 0 }),
-            copy({
-                targets: [{ src: 'assets', dest: 'dist' }],
-            }),
-        ],
-    },
-];
+//         plugins: [
+//             //
+//             typescript(),
+//             esbuild({
+//                 target: 'es6',
+//                 loaders: 'ts',
+//                 exclude: 'node_modules',
+//                 platform: 'browser',
+//             }),
+//             // serve({ port: 8080 }),
+//             // livereload({ delay: 0 }),
+//             // copy({
+//             //     targets: [{ src: 'assets', dest: 'dist' }],
+//             // }),
+//         ],
+//     },
+// ];
